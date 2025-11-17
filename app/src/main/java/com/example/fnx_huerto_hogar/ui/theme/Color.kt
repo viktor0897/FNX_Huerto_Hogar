1225fbf1e8ff4904d238193ed0a2cc2e440f7e2f
Victor Alvarado <vi.alvaradom@duocuc.cl>
2025-11-16 22:49:41 -0400

    Colores y temas

--- app/src/main/java/com/example/fnx_huerto_hogar/ui/theme/Color.kt
+++ app/src/main/java/com/example/fnx_huerto_hogar/ui/theme/Color.kt
@@ -1,11 +1,28 @@
 package com.example.fnx_huerto_hogar.ui.theme
 
+//import androidx.compose.ui.graphics.Color
+//
+//val Purple80 = Color(0xFFD0BCFF)
+//val PurpleGrey80 = Color(0xFFCCC2DC)
+//val Pink80 = Color(0xFFEFB8C8)
+//
+//val Purple40 = Color(0xFF6650a4)
+//val PurpleGrey40 = Color(0xFF625b71)
+//val Pink40 = Color(0xFF7D5260)
+
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+//Fondo general
+val GrayBackground = Color(0xFFF7F7F7)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+//Color principal (Botones, iconos activos, títulos importantes).
+val GreenPrimary = Color(0xFF226B42)
+
+//Verde para etiquetas pequeñas y fondos suaves.
+val GreenSecondary = Color(0x33226B42)
+
+//Café para bordes, divisores y detalles.
+val BrownSecondary = Color(0xFF753A10)
+
+//Amarillo para precio, ofertas y acentos importantes
+val YellowAccent = Color(0xFFFFDD70)
