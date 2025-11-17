4d46c62ccdf745ee69cd494f815999f3e2aa268f
Victor Alvarado <vi.alvaradom@duocuc.cl>
2025-11-16 21:51:06 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/fnx_huerto_hogar/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.fnx_huerto_hogar
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.fnx_huerto_hogar.ui.theme.FNX_Huerto_HogarTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FNX_Huerto_HogarTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FNX_Huerto_HogarTheme {
+        Greeting("Android")
+    }
+}
