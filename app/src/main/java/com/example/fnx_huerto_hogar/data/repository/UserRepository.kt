2202e63ccb2df1aaec052222cd4fb1b792053c9d
Victor Alvarado <vi.alvaradom@duocuc.cl>
2025-11-23 00:00:05 -0400

    User, lógica completa

---
+++ app/src/main/java/com/example/fnx_huerto_hogar/data/repository/UserRepository.kt
@@ -0,0 +1,83 @@
+package com.example.fnx_huerto_hogar.data.repository
+
+import com.example.fnx_huerto_hogar.data.model.User
+import com.example.fnx_huerto_hogar.data.model.UserRole
+import kotlinx.coroutines.delay
+
+class UserRepository {
+
+    suspend fun registerUser(user: User): Boolean {
+        delay(1000)
+        return try {
+            val userExist = users.find { it.email == user.email }
+            if (userExist != null) {
+                return false // Usuario Existe
+            } else {
+                users.add(user)
+                return true
+            }
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    //Login
+    suspend fun login(email: String, password: String): User? {
+        delay(800)
+        return users.find { it.email == email && it.password == password }
+    }
+
+    //Coger todos
+    suspend fun getAllUsers(): List<User> {
+        delay(500)
+        return users.toList()
+    }
+
+    //Deletear
+    suspend fun deleteUser(email: String): Boolean {
+        delay(300)
+        return try {
+            val userExist = users.find { it.email == email }
+            if (userExist != null) {
+                users.remove(userExist)
+                true
+            } else {
+                false
+            }
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    //Cogerlos por Email
+    suspend fun getUserByEmail(email: String): User?{
+        delay(400)
+        return users.find {it.email == email}
+    }
+
+    //Lista de usuarios seteados
+    private val users = mutableListOf<User>(
+        User(
+            email = "bastian@duoc.com",
+            name = "Bastian",
+            lastName = "Burgos",
+            password = "123456",
+            phone = "912345678",
+            address = "Av. Principal 123",
+            comuna = "Santiago",
+            region = "Región Metropolitana",
+            rol = UserRole.USER
+        ),
+        User(
+            email = "eriberto@gmail.com",
+            name = "Eriberto",
+            lastName = "Moya",
+            password = "123456789",
+            phone = "987654321",
+            address = "Calle Secundaria 456",
+            comuna = "Providencia",
+            region = "Región Metropolitana",
+            rol = UserRole.USER
+        )
+    )
+}
